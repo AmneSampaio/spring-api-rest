50caeee74b59fe2b4cf2da782130b7e65cee5ded
AmneSampaio <amnesf@gmail.com>
2021-04-15 14:48:40 -0300

    Alterando endpoints de cadastro

--- src/main/java/com/concrete/projeto/refactor/controller/UserController.java
+++ src/main/java/com/concrete/projeto/refactor/controller/UserController.java
@@ -1,21 +1,31 @@
 package com.concrete.projeto.refactor.controller;
 
+import com.concrete.projeto.refactor.model.JwtRequest;
+import com.concrete.projeto.refactor.model.JwtResponse;
 import com.concrete.projeto.refactor.model.Phone;
 import com.concrete.projeto.refactor.model.User;
 import com.concrete.projeto.refactor.repository.UserRepository;
+import com.concrete.projeto.refactor.service.UserService;
+import com.concrete.projeto.refactor.utility.JWTUtility;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
@@ -25,58 +35,53 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    public static BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    };
-
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<User> findById(@PathVariable("id") Long id) {
+    public ResponseEntity<User> findById(@PathVariable("id") UUID id) {
         Optional<User> userDB = userRepository.findById(id);
 
-
-
-
-        if (!userDB.isPresent()) { return ResponseEntity.notFound().build(); }
+        if (!userDB.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
 
         return ResponseEntity.ok(userDB.get());
     }
 
-    @PostMapping
+    @PostMapping("/signIn")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiResponses( value = {
-            @ApiResponse(code = 201, message = "Successfully"),
-            @ApiResponse(code = 400, message = "An error has occurred creating the user")
-    })
-    public ResponseEntity<?> create(@RequestBody User user) {
-            user.cryptPassword();
-        return ResponseEntity.ok().body(userRepository.save(user));
+    public ResponseEntity<?> create(@Valid @RequestBody User user) throws Exception {
+        Optional<User> userExistsByEmail = userRepository.findByEmail(user.getEmail());
+
+        if (userExistsByEmail.isPresent()) {
+            return ResponseEntity.badRequest().build();
+        } else {
+            //user.cryptPassword();
+            return ResponseEntity.ok(userRepository.save(user));
+        }
+
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    @ApiResponses( value = {
-            @ApiResponse(code = 201, message = "Successfully"),
-            @ApiResponse(code = 400, message = "An error has occurred creating the user")
-    })
-    public ResponseEntity<?> login(@Valid @NotBlank @NotNull @RequestBody User user) {
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> login(@Valid @RequestBody User user) {
         Optional<User> lookForEmailOnDB = userRepository.findByEmail(user.getEmail());
 
-        if (lookForEmailOnDB.isPresent()) {
+        if (lookForEmailOnDB.isPresent() && lookForEmailOnDB.get().getEmail().equals(user.getEmail())) {
 
-            String lookForPasswordCrypt = passwordEncoder().encode(user.getPassword());
-            if (lookForEmailOnDB.get().getEmail().equals(user.getEmail())) {
-                lookForEmailOnDB.get().s
-            }
-        return ResponseEntity.ok().body(userRepository.save(user));
+            /*String lookForPasswordCrypt = passwordEncoder().encode(user.getPassword());
+            if (lookForEmailOnDB.get().getPassword().equals(lookForPasswordCrypt)) {*/
+
+            lookForEmailOnDB.get().setLastUserLogin(LocalDateTime.now());
+            userRepository.save(lookForEmailOnDB.get());
+        }
+
+
+        return new ResponseEntity<User>(lookForEmailOnDB.get(),HttpStatus.OK);
+
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    @ApiResponses( value = {
-            @ApiResponse(code = 201, message = "Successfully"),
-            @ApiResponse(code = 400, message = "An error has occurred updating the user")
-    })
     public ResponseEntity<User> update(@PathVariable("id") Long id,@RequestBody User user) {
 
         Optional<User> userFoundById = userRepository.findById(id);
@@ -98,10 +103,6 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ApiResponses(value = {
-            @ApiResponse(code = 204, message = "Successfully removed"),
-            @ApiResponse(code = 404, message = "Register not found")
-    })
     public ResponseEntity<?> remove(@PathVariable Long id) {
         Optional<User> userID = userRepository.findById(id);
         if (!userID.isPresent()) { return ResponseEntity.notFound().build(); }
@@ -110,12 +111,14 @@ public class UserController {
         return ResponseEntity.noContent().build();
 
     }
+/*
 
-    private boolean validateSignIn(String email, String login) throws Exception {
-        if (userRepository.existsByEmailOrLogin(email, login)) {
+    private boolean validateSignIn(String email) throws Exception {
+        if (userRepository.existsByEmail(email)) {
             throw new Exception("Email already in use.");
         }
         return true;
     }
+*/
 
 }
