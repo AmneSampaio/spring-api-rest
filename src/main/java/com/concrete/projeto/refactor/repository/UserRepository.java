b740437a05f9e3c561d3c6b2a542413850316129
AmneSampaio <amnesf@gmail.com>
2021-04-14 23:43:50 -0300

    Retirando método obsoleto

--- src/main/java/com/concrete/projeto/refactor/repository/UserRepository.java
+++ src/main/java/com/concrete/projeto/refactor/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.concrete.projeto.refactor.repository;
 
 
-import com.concrete.projeto.refactor.model.Address;
 import com.concrete.projeto.refactor.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
@@ -9,6 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String name);
     Optional<User> findByEmail(String email);
-    Optional<User> findByAddress(Address address);
-    boolean existsByCpfOrLogin(String cpf, String login);
+    boolean existsByEmailOrLogin(String email, String login);
 }
