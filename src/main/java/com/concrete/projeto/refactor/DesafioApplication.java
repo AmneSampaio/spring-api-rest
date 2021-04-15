1a8cd331f28dad2f0b0bb90f5d97b13634b20434
AmneSampaio <amnesf@gmail.com>
2021-04-15 14:53:26 -0300

    Adicionando PasswordEncoder ao subir a api (temporario)

--- src/main/java/com/concrete/projeto/refactor/DesafioApplication.java
+++ src/main/java/com/concrete/projeto/refactor/DesafioApplication.java
@@ -2,6 +2,9 @@ package com.concrete.projeto.refactor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DesafioApplication {
@@ -10,4 +13,8 @@ public class DesafioApplication {
 		SpringApplication.run(DesafioApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
