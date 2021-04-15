c434f9242da98b35e979cb3ff9545cbd0d9c4161
AmneSampaio <amnesf@gmail.com>
2021-04-15 14:51:56 -0300

    Adicionando findById e existsByEmail

--- src/main/java/com/concrete/projeto/refactor/repository/UserRepository.java
+++ src/main/java/com/concrete/projeto/refactor/repository/UserRepository.java
@@ -4,9 +4,12 @@ package com.concrete.projeto.refactor.repository;
 import com.concrete.projeto.refactor.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String name);
     Optional<User> findByEmail(String email);
-    boolean existsByEmailOrLogin(String email, String login);
+    Optional<User> findById(UUID id);
+    boolean existsByEmail(String email);
+
 }
